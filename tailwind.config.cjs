64bc2b0ece856b0e16800cd661fe6dd70e36e2bb
Nicolas Arevalo <inag.458@gmail.com>
2023-03-13 18:26:53 -0500

    Final push

---
+++ tailwind.config.cjs
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		colors: {
+			fucsia: '#E1028F',
+			azulito: '#03b9e8',
+			bgcolor: {
+				333: '#333333',
+				27: '#272727',
+			},
+			blanco: '#FFF',
+			morado: '#8858ee',
+			bgabout: '#202020',
+			gris: '#ada5aa'
+		},
+		extend: {},
+	},
+	plugins: [],
+}
